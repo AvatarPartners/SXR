8be9c2f5298e0e5b01655f7e897b58d87d474619
JonathanWindows <jmartin@avatarpartners.com>
2020-03-30 16:10:11 -0400

    Initializer Error Fix

--- Editor/Initializer/SimplifyXRExtensionsInitializer.cs
+++ Editor/Initializer/SimplifyXRExtensionsInitializer.cs
@@ -1,9 +1,9 @@
-﻿using UnityEditor;
-using UnityEngine;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UnityEditor;
+using UnityEngine;
 #if USING_ARFOUNDATION && SIMPLIFYXR_ARFOUNDATION_PRESENT
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -11,6 +11,7 @@ using UnityEngine.XR.ARSubsystems;
 
 namespace SimplifyXR
 {
+#if UNITY_EDITOR
     /// <summary>
     /// Sets and prepares editor functionality for presence of 3rd party extensions
     /// DO NOT MOVE, REMOVE OR MODIFY.
@@ -18,51 +19,51 @@ namespace SimplifyXR
     [InitializeOnLoad]
     public static class SimplifyXRExtensionsInitializer
     {
-        static SimplifyXRExtensionsInitializer()
+        static SimplifyXRExtensionsInitializer ()
         {
-            SetVisionLibEditorExtensions();
-            SetARFoundationEditorExtensions();
+            SetVisionLibEditorExtensions ();
+            SetARFoundationEditorExtensions ();
         }
 
-        static void SetVisionLibEditorExtensions()
+        static void SetVisionLibEditorExtensions ()
         {
 #if USING_VISIONLIB
 
-            SimplifyXRAccessManager.Instance.SetVisionLibEditorExtensionBehavior(new VisionLibEditorExtensionBehavior());
+            SimplifyXRAccessManager.Instance.SetVisionLibEditorExtensionBehavior (new VisionLibEditorExtensionBehavior ());
 
             SimplifyXRPostprocessor.SimplifyXRVisionLibPresent = true;
-            SimplifyXRAccessManager.Instance.SetVisionLibExtensionBehavior(new VisionLibEditorDLLExtensionBehavior());
-            CheckVisionLibVersion();
+            SimplifyXRAccessManager.Instance.SetVisionLibExtensionBehavior (new VisionLibEditorDLLExtensionBehavior ());
+            CheckVisionLibVersion ();
 #else
             SimplifyXRPostprocessor.SimplifyXRVisionLibPresent = false;
 #endif
         }
 
-        static void SetARFoundationEditorExtensions()
+        static void SetARFoundationEditorExtensions ()
         {
 #if USING_ARFOUNDATION && SIMPLIFYXR_ARFOUNDATION_PRESENT
-            SimplifyXRAccessManager.Instance.SetARFoundationEditorExtensionBehavior(new ARFoundationEditorExtensionBehavior());
+            SimplifyXRAccessManager.Instance.SetARFoundationEditorExtensionBehavior (new ARFoundationEditorExtensionBehavior ());
 #endif
         }
 
-        static void CheckVisionLibVersion()
+        static void CheckVisionLibVersion ()
         {
 #if USING_VISIONLIB
             Version installedVisionLibVersion;
-            if (VLUnitySdk.GetVersionString(out string version))
-                installedVisionLibVersion = new Version(version);
+            if (VLUnitySdk.GetVersionString (out string version))
+                installedVisionLibVersion = new Version (version);
             else
-                installedVisionLibVersion = new Version(SimplifyXRAccessManager.Instance.GetSupportedVisionLibVersion());
+                installedVisionLibVersion = new Version (SimplifyXRAccessManager.Instance.GetSupportedVisionLibVersion ());
 
-            var supportedVisionLibVersion = new Version(SimplifyXRAccessManager.Instance.GetSupportedVisionLibVersion());
+            var supportedVisionLibVersion = new Version (SimplifyXRAccessManager.Instance.GetSupportedVisionLibVersion ());
 
             if (installedVisionLibVersion != null && supportedVisionLibVersion != null)
             {
-                var result = installedVisionLibVersion.CompareTo(supportedVisionLibVersion);
+                var result = installedVisionLibVersion.CompareTo (supportedVisionLibVersion);
                 if (result > 0)
-                    Debug.LogWarningFormat("WARNING: Your installed version ({0}) of VisionLib is ahead of the SimplifyXR supported version ({1}) of VisionLib", installedVisionLibVersion, supportedVisionLibVersion);
+                    Debug.LogWarningFormat ("WARNING: Your installed version ({0}) of VisionLib is ahead of the SimplifyXR supported version ({1}) of VisionLib", installedVisionLibVersion, supportedVisionLibVersion);
                 else if (result < 0)
-                    Debug.LogWarningFormat("WARNING: Your installed version ({0}) of VisionLib is behind the SimplifyXR supported version ({1}) of VisionLib", installedVisionLibVersion, supportedVisionLibVersion);
+                    Debug.LogWarningFormat ("WARNING: Your installed version ({0}) of VisionLib is behind the SimplifyXR supported version ({1}) of VisionLib", installedVisionLibVersion, supportedVisionLibVersion);
             }
 #endif
         }
@@ -71,42 +72,42 @@ namespace SimplifyXR
     class VisionLibEditorExtensionBehavior : IManageVisionLibEditorExtensions
     {
         #region IManageVisionLibEditorExtensions implementation
-        public void ConfigureVLWorkerBehaviour(GameObject go, string visionLibLicenseFileName, string visionLibCameraCalFileName)
+        public void ConfigureVLWorkerBehaviour (GameObject go, string visionLibLicenseFileName, string visionLibCameraCalFileName)
         {
 #if USING_VISIONLIB
             if (go != null)
             {
-                var workerBehavior = go.GetComponent<VLWorkerBehaviour>();
+                var workerBehavior = go.GetComponent<VLWorkerBehaviour> ();
                 if (workerBehavior != null)
                 {
-                    if (!string.IsNullOrEmpty(visionLibLicenseFileName))
+                    if (!string.IsNullOrEmpty (visionLibLicenseFileName))
                         workerBehavior.licenseFile.path = visionLibLicenseFileName;
-                    if (!string.IsNullOrEmpty(visionLibCameraCalFileName))
+                    if (!string.IsNullOrEmpty (visionLibCameraCalFileName))
                         workerBehavior.calibrationDataBaseURI = "project_dir:/" + visionLibCameraCalFileName;
                     workerBehavior.targetFPS = 30;
                     return;
                 }
             }
-            Debug.LogError("Check the VLWorkerBehaviour on the VLHoloLensTracker or VLCamera prefab as there were issues configuring it.");
+            Debug.LogError ("Check the VLWorkerBehaviour on the VLHoloLensTracker or VLCamera prefab as there were issues configuring it.");
 #endif
         }
 
-        public void ConfigureVLHololensTrackerBehaviour(GameObject go, GameObject contentHolder)
+        public void ConfigureVLHololensTrackerBehaviour (GameObject go, GameObject contentHolder)
         {
 #if USING_VISIONLIB
             if (go != null)
             {
-                var hololensStabilizationBehavior = go.GetComponent<VLHoloLensStabilizationPlaneBehaviour>();
+                var hololensStabilizationBehavior = go.GetComponent<VLHoloLensStabilizationPlaneBehaviour> ();
                 if (hololensStabilizationBehavior != null)
                 {
-                    UnityEngine.Object.DestroyImmediate(hololensStabilizationBehavior);
+                    UnityEngine.Object.DestroyImmediate (hololensStabilizationBehavior);
                 }
-                var vLDetectScreenChange = go.GetComponent<VLDetectScreenChangeBehaviour>();
+                var vLDetectScreenChange = go.GetComponent<VLDetectScreenChangeBehaviour> ();
                 if (vLDetectScreenChange != null)
                 {
-                    UnityEngine.Object.DestroyImmediate(vLDetectScreenChange);
+                    UnityEngine.Object.DestroyImmediate (vLDetectScreenChange);
                 }
-                var hololensTrackerBehavior = go.GetComponent<VLHoloLensTrackerBehaviour>();
+                var hololensTrackerBehavior = go.GetComponent<VLHoloLensTrackerBehaviour> ();
                 if (hololensTrackerBehavior != null)
                 {
                     hololensTrackerBehavior.smoothTime = 0f;
@@ -117,21 +118,21 @@ namespace SimplifyXR
                     }
                 }
             }
-            Debug.LogError("Check the VLHoloLensTracker prefab as there were issues configuring it.");
+            Debug.LogError ("Check the VLHoloLensTracker prefab as there were issues configuring it.");
 #endif
         }
 
-        public void ConfigureVLHololensInitCamera(GameObject go)
+        public void ConfigureVLHololensInitCamera (GameObject go)
         {
 #if USING_VISIONLIB
             if (go != null)
             {
-                var hololensInitCameraBehavior = go.GetComponent<VLHoloLensInitCameraBehaviour>();
+                var hololensInitCameraBehavior = go.GetComponent<VLHoloLensInitCameraBehaviour> ();
                 if (hololensInitCameraBehavior != null)
                 {
                     hololensInitCameraBehavior.keepUpright = true;
                 }
-                var cam = go.GetComponent<Camera>();
+                var cam = go.GetComponent<Camera> ();
                 if (cam != null)
                 {
                     cam.nearClipPlane = 0.3f;
@@ -141,36 +142,36 @@ namespace SimplifyXR
                 }
                 return;
             }
-            Debug.LogError("Check the VLHoloLensInitCamera prefab as there were issues configuring it.");
+            Debug.LogError ("Check the VLHoloLensInitCamera prefab as there were issues configuring it.");
 #endif
         }
         #endregion
     }
     class ARFoundationEditorExtensionBehavior : IManageARFoundationEditorExtensions
     {
-        public void AddARFoundationAdapterComponent(GameObject contentHolder, bool TrackAllTargetsInDatabase, string IndividualTargetToTrack, ScriptableObject imageDatabase, GameObject sessionOrigin, bool IsPlanar)
+        public void AddARFoundationAdapterComponent (GameObject contentHolder, bool TrackAllTargetsInDatabase, string IndividualTargetToTrack, ScriptableObject imageDatabase, GameObject sessionOrigin, bool IsPlanar)
         {
 #if USING_ARFOUNDATION && SIMPLIFYXR_ARFOUNDATION_PRESENT
             if (IsPlanar)
             {
-                ARFoundationAdapterPlanes adapter = sessionOrigin.AddComponent<ARFoundationAdapterPlanes>();
+                ARFoundationAdapterPlanes adapter = sessionOrigin.AddComponent<ARFoundationAdapterPlanes> ();
                 adapter.contentHolder = contentHolder;
                 adapter.stopMovingAfterFirstHit = true;
             }
             else
             {
-                ARFoundationAdapter adapter = sessionOrigin.AddComponent<ARFoundationAdapter>();
+                ARFoundationAdapter adapter = sessionOrigin.AddComponent<ARFoundationAdapter> ();
 
                 adapter.contentHolder = contentHolder;
                 adapter.TrackAll = TrackAllTargetsInDatabase;
 
                 bool nameInReferenceLibrary = false;
 
-                if (imageDatabase.GetType().IsAssignableFrom(typeof(XRReferenceImageLibrary)))
+                if (imageDatabase.GetType ().IsAssignableFrom (typeof (XRReferenceImageLibrary)))
                 {
-                    foreach (XRReferenceImage image in (XRReferenceImageLibrary)imageDatabase)
+                    foreach (XRReferenceImage image in (XRReferenceImageLibrary) imageDatabase)
                     {
-                        if (image.name.Equals(IndividualTargetToTrack))
+                        if (image.name.Equals (IndividualTargetToTrack))
                         {
                             adapter.defaultTexture = image.texture;
                             nameInReferenceLibrary = true;
@@ -179,75 +180,64 @@ namespace SimplifyXR
                 }
                 else
                 {
-                    EditorUtility.DisplayDialog("Image Library Missing", "You did not assign an ImageLibrary. Locate teh ARTrackedImageManager to manually assign this object", "Ok");
+                    EditorUtility.DisplayDialog ("Image Library Missing", "You did not assign an ImageLibrary. Locate teh ARTrackedImageManager to manually assign this object", "Ok");
                 }
 
                 if (!nameInReferenceLibrary && !TrackAllTargetsInDatabase)
-                    EditorUtility.DisplayDialog("Image not in Library", "The image named " + IndividualTargetToTrack + " is not in the Reference Library. This image will not be tracked until added to the Reference Library", "OK");
+                    EditorUtility.DisplayDialog ("Image not in Library", "The image named " + IndividualTargetToTrack + " is not in the Reference Library. This image will not be tracked until added to the Reference Library", "OK");
 
                 adapter.ImageTargetName = IndividualTargetToTrack;
             }
 #endif
         }
 
-        public void CreateARSession()
+        public void CreateARSession ()
         {
 #if USING_ARFOUNDATION && SIMPLIFYXR_ARFOUNDATION_PRESENT
-Debug.Log("Creating AR Session");
-            EditorApplication.ExecuteMenuItem("GameObject/XR/AR Session");
+            EditorApplication.ExecuteMenuItem ("GameObject/XR/AR Session");
 #endif
         }
 
-        public GameObject CreateARSessionOrigin(int targetType, ScriptableObject imageDatabase)
+        public GameObject CreateARSessionOrigin (int targetType, ScriptableObject imageDatabase)
         {
 #if USING_ARFOUNDATION && SIMPLIFYXR_ARFOUNDATION_PRESENT
-Debug.Log("Creating AR Session Origin");
-            EditorApplication.ExecuteMenuItem("GameObject/XR/AR Session Origin");
-            GameObject sessionOrigin = GameObject.FindObjectOfType<ARSessionOrigin>().gameObject;
-Debug.Log("Origin Created");
+            EditorApplication.ExecuteMenuItem ("GameObject/XR/AR Session Origin");
+            GameObject sessionOrigin = GameObject.FindObjectOfType<ARSessionOrigin> ().gameObject;
 
             if (targetType == 0)
             {
-                Debug.Log("Adding Referenec Image Library");
+                ARTrackedImageManager imageManager = sessionOrigin.AddComponent<ARTrackedImageManager> ();
 
-                ARTrackedImageManager imageManager = sessionOrigin.AddComponent<ARTrackedImageManager>();
-
-                if (imageDatabase.GetType().IsAssignableFrom(typeof(XRReferenceImageLibrary)))
+                if (imageDatabase.GetType ().IsAssignableFrom (typeof (XRReferenceImageLibrary)))
                 {
                     imageManager.referenceLibrary = imageDatabase as XRReferenceImageLibrary;
                     imageManager.maxNumberOfMovingImages = 1;
                 }
                 else
                 {
-                    EditorUtility.DisplayDialog("Image Library Missing", "You did not assign an ImageLibrary. Locate teh ARTrackedImageManager to manually assign this object", "Ok");
+                    EditorUtility.DisplayDialog ("Image Library Missing", "You did not assign an ImageLibrary. Locate teh ARTrackedImageManager to manually assign this object", "Ok");
                 }
 
             }
             else if (targetType == 1)
             {
-                Debug.Log("Adding image");
-
-                ARPlaneManager planeManager = sessionOrigin.AddComponent<ARPlaneManager>();
-                GameObject prefab = SimplifyXREditorUtilityMethods.FindPrefabInAssets("SXR Default Plane");
+                ARPlaneManager planeManager = sessionOrigin.AddComponent<ARPlaneManager> ();
+                GameObject prefab = SimplifyXREditorUtilityMethods.FindPrefabInAssets ("SXR Default Plane");
                 if (prefab != null)
                     planeManager.planePrefab = prefab;
                 else
-                    SimplifyXRDebug.SimplifyXRLog(SimplifyXRDebug.Type.Error, "[DEVELOPER ERROR] No SXR Default Plane prefab in project", SimplifyXRDebug.Args());
+                    SimplifyXRDebug.SimplifyXRLog (SimplifyXRDebug.Type.Error, "[DEVELOPER ERROR] No SXR Default Plane prefab in project", SimplifyXRDebug.Args ());
             }
 
-    Debug.Log("Adding RaycastManager");
+            sessionOrigin.AddComponent<ARRaycastManager> ();
 
-            sessionOrigin.AddComponent<ARRaycastManager>();
-
-            Camera theCam = sessionOrigin.GetComponentInChildren<Camera>();
+            Camera theCam = sessionOrigin.GetComponentInChildren<Camera> ();
             theCam.tag = "MainCamera";
-            
-            Debug.Log("origin Operations complete");
-
             return sessionOrigin;
 #else
             return null;
 #endif
         }
     }
+#endif
 }
